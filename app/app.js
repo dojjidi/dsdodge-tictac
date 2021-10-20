96a5837d85e872017889a3c1b1f73c8019d90290
dojjidi <dojjidi@gmail.com>
2021-10-20 17:18:42 -0600

    add references

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-const authEvents = require('./auth/events')
+// const authEvents = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
-
 $(() => {
+  // Whenever our #sign-up form is submitted, call the `onSignUp` function
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#sign-out').on('click', authEvents.onSignOut)
