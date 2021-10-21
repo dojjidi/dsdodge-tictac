9d1559a3960b6858f122191620d3972b6ca1be8c
dojjidi <dojjidi@gmail.com>
2021-10-20 18:46:59 -0600

    update

--- app/events.js
+++ app/events.js
@@ -1,49 +1,32 @@
-// get them form fields
-const getFormFields = require('../lib/get-form-fields')
-// require api auth functions
-const api = require('./api')
-// require ui functions to update page
-const ui = require('./ui')
-// sign up function events
-const onSignUp = function (event) {
-  // prevents page from refreshing
-  event.preventDefault()
-  // target 'submit' event
-  const form = event.target
-  // get data from form
-  const formData = getFormFields(form)
-  // post sign in request
-  api
-    .signUp(formData)
-    // return singUpSuccess on success
-    .then(ui.signUpSuccess)
-    // otherwise return error
-    .catch(ui.signUpFailure)
-}
+'use strict'
 
-const onSignIn = function (event) {
-  event.preventDefault()
-  const form = event.target
-  const formData = getFormFields(form)
-  api
-    .signIn(formData)
-    .then(ui.signInSuccess)
-    .catch(ui.signUpFailure)
+// const getFormFields = require('../../lib/get-form-fields')
+// // const api = require('.')
 
-  api
-    .signIn(formData)
-    .then(ui.signInSuccess)
-    .catch(ui.signInFailure)
-}
+// const onSignUp = function (event) {
+//   event.preventDefault()
+//   const form = event.target
+//   const formData = getFormFields(form)
+//   api.signUp(formData)
+//     .then(ui.signUpSuccess)
+//     .catch(ui.signUpFailure)
+// }
 
-const onSignOut = function () {
-  api.signOut()
-    .then(ui.signOutSuccess)
-    .catch(ui.signOutFailure)
-}
+const x = 'x'
+const o = 'circle'
+const winCons = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
-module.exports = {
-  onSignUp,
-  onSignIn,
-  onSignOut
-}
+// module.exports = {
+//   onSignUp,
+//   onSignIn,
+//   onChangePassword,
+//   onSignOut
+// }
